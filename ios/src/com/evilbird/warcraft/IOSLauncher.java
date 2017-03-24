b91649de649df69402c15d04d2e57ebd2862ddef
Blair Butterworth <blair.butterworth@gmail.com>
2017-03-24 08:25:46 +0000

    Renamed file to match class name

--- ios/src/com/evilbird/warcraft/IOSLauncher.java
+++ ios/src/com/evilbird/warcraft/IOSLauncher.java
@@ -1,10 +1,6 @@
 package com.evilbird.warcraft;
 
-import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
-import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
-import com.evilbird.engine.device.Device;
-import com.evilbird.engine.game.GameEngine;
 
 import org.robovm.apple.foundation.NSAutoreleasePool;
 import org.robovm.apple.uikit.UIApplication;
@@ -12,10 +8,13 @@ import org.robovm.apple.uikit.UIApplication;
 public class IOSLauncher extends IOSApplication.Delegate {
     @Override
     protected IOSApplication createApplication() {
+        /*
         Device device = new IOSDevice();
         ApplicationListener delegate = new GameEngine(device);
         IOSApplicationConfiguration configuration = new IOSApplicationConfiguration();
         return new IOSApplication(delegate, configuration);
+        */
+        throw new UnsupportedOperationException();
     }
 
     public static void main(String[] argv) {
