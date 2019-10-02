028ad559bfdaa343abed903594586f5e7c982121
Blair <blair.butterworth@gmail.com>
2019-10-02 13:59:07 +0100

    Added enums for team allegiance and colour

---
+++ core/src/com/evilbird/warcraft/common/TeamColour.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2019, Blair Butterworth
+ *
+ * This work is licensed under the MIT License. To view a copy of this
+ * license, visit
+ *
+ *        https://opensource.org/licenses/MIT
+ */
+
+package com.evilbird.warcraft.common;
+
+public enum TeamColour
+{
+    Blue,
+    None,
+    Orange,
+    Purple,
+    Red,
+    White,
+}
