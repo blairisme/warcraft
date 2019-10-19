0c77258e280f5b55338651a75bbf20e6f40eabce
Blair <blair.butterworth@gmail.com>
2019-10-19 16:51:42 +0100

    Added caching to interaction lookup

--- core/src/com/evilbird/warcraft/behaviour/ui/interaction/Interactions.java
+++ core/src/com/evilbird/warcraft/behaviour/ui/interaction/Interactions.java
@@ -9,8 +9,15 @@
 
 package com.evilbird.warcraft.behaviour.ui.interaction;
 
+import com.evilbird.engine.device.UserInput;
+import com.evilbird.engine.device.UserInputType;
+import com.evilbird.engine.item.Item;
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
 import javax.inject.Inject;
-import javax.inject.Provider;
 
 /**
  * Instances of this class define the different ways the user can interact with
@@ -18,11 +25,14 @@ import javax.inject.Provider;
  *
  * @author Blair Butterworth
  */
-public class Interactions extends InteractionContainer
+public class Interactions
 {
+    private InteractionContainer interactions;
+    private Cache<InteractionQuery, InteractionQueryResult> cache;
+
     @Inject
     public Interactions(
-        Provider<InteractionDefinition> factory,
+        InteractionContainer interactions,
         AttackInteractions attackInteractions,
         CameraInteractions cameraInteractions,
         CheatInteractions cheatInteractions,
@@ -34,16 +44,87 @@ public class Interactions extends InteractionContainer
         SelectInteractions selectInteractions,
         TransportInteractions transportInteractions)
     {
-        super(factory);
-        addActions(moveInteractions);
-        addActions(attackInteractions);
-        addActions(gatherInteractions);
-        addActions(constructInteractions);
-        addActions(produceInteractions);
-        addActions(transportInteractions);
-        addActions(menuInteractions);
-        addActions(cameraInteractions);
-        addActions(selectInteractions);
-        addActions(cheatInteractions);
+        this.interactions = interactions;
+        this.interactions.addActions(moveInteractions);
+        this.interactions.addActions(attackInteractions);
+        this.interactions.addActions(gatherInteractions);
+        this.interactions.addActions(constructInteractions);
+        this.interactions.addActions(produceInteractions);
+        this.interactions.addActions(transportInteractions);
+        this.interactions.addActions(menuInteractions);
+        this.interactions.addActions(cameraInteractions);
+        this.interactions.addActions(selectInteractions);
+        this.interactions.addActions(cheatInteractions);
+        this.cache = Caffeine.newBuilder().maximumSize(100).build();
+    }
+
+    /**
+     * Returns the first interaction that applies to the given user input, target
+     * item and selected item. The specific matching algorithm is contained in
+     * the individual {@link Interaction} implementations.
+     *
+     * @param input     a {@link UserInput} instance specifying the interaction
+     *                  the user made with the application
+     * @param item      the target of the users interaction.
+     * @param selected  a currently selected item.
+     *
+     * @return the matching {@code Interaction}, if any.
+     */
+    public Interaction getInteraction(UserInput input, Item item, Item selected) {
+        InteractionQuery query = new InteractionQuery(input, item, selected);
+        InteractionQueryResult result = cache.getIfPresent(query);
+        if (result == null) {
+            Interaction interaction = interactions.getInteraction(input, item, selected);
+            result = new InteractionQueryResult(interaction);
+            cache.put(query, result);
+        }
+        return result.getValue();
+    }
+
+    private static class InteractionQueryResult {
+        private Interaction value;
+
+        public InteractionQueryResult(Interaction value) {
+            this.value = value;
+        }
+
+        public Interaction getValue() {
+            return value;
+        }
+    }
+
+    private static class InteractionQuery {
+        private UserInputType type;
+        private Item target;
+        private Item selected;
+
+        public InteractionQuery(UserInput input, Item target, Item selected) {
+            this.type = input.getType();
+            this.target = target;
+            this.selected = selected;
+        }
+
+        @Override
+        public boolean equals(Object object) {
+            if (object == null) { return false; }
+            if (object == this) { return true; }
+            if (object.getClass() != getClass()) { return false; }
+
+            InteractionQuery that = (InteractionQuery)object;
+            return new EqualsBuilder()
+                .append(type, that.type)
+                .append(target, that.target)
+                .append(selected, that.selected)
+                .isEquals();
+        }
+
+        @Override
+        public int hashCode() {
+            return new HashCodeBuilder(17, 37)
+                .append(type)
+                .append(target)
+                .append(selected)
+                .toHashCode();
+        }
     }
 }
