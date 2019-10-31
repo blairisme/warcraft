eb5583f8b70509bc64888b00be4113019337d923
Blair <blair.butterworth@gmail.com>
2019-10-31 15:59:04 +0000

    Added invisibilty spell implementation

---
+++ core/src/com/evilbird/warcraft/action/spell/buff/InvisibilityCancel.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2019, Blair Butterworth
+ *
+ * This work is licensed under the MIT License. To view a copy of this
+ * license, visit
+ *
+ *        https://opensource.org/licenses/MIT
+ */
+
+package com.evilbird.warcraft.action.spell.buff;
+
+import com.evilbird.engine.action.framework.BasicAction;
+import com.evilbird.warcraft.item.unit.combatant.Combatant;
+
+import javax.inject.Inject;
+
+import static com.evilbird.engine.action.ActionConstants.ActionComplete;
+
+/**
+ * An action that removes the effects of the invisibility spell.
+ *
+ * @author Blair Butterworth
+ */
+public class InvisibilityCancel extends BasicAction
+{
+    @Inject
+    public InvisibilityCancel() {
+    }
+
+    @Override
+    public boolean act(float delta) {
+        Combatant target = (Combatant)getTarget();
+        target.setAttackable(true);
+        return ActionComplete;
+    }
+}
